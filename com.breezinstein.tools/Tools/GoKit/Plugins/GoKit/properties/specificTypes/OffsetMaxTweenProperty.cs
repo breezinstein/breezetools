1b78c6a491aedf5f8f96ca97002ed8c46ba37d52
Adedeji Ogundipe <ogundipeadedeji@gmail.com>
2023-05-20 21:45:15 +0100

    removed GoKit

--- com.breezinstein.tools/Tools/GoKit/Plugins/GoKit/properties/specificTypes/OffsetMaxTweenProperty.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public sealed class OffsetMaxTweenProperty : AbstractOffsetTweenProperty
-{
-    public OffsetMaxTweenProperty( Vector2 endValue, bool isRelative = false ) : base( endValue, isRelative, useMax: true )
-    {
-    }
-}
