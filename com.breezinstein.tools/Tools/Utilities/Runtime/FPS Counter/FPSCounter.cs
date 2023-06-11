e13f6c55cb30785f4d2da91d4753a57b55fe7078
Adedeji Ogundipe <ogundipeadedeji@gmail.com>
2023-06-11 20:22:53 +0100

    Update FPSCounter.cs

--- com.breezinstein.tools/Tools/Utilities/Runtime/FPS Counter/FPSCounter.cs
+++ com.breezinstein.tools/Tools/Utilities/Runtime/FPS Counter/FPSCounter.cs
@@ -1,24 +1,29 @@
 using System;
+using System.Text;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Breezinstein.Tools
 {
-    [RequireComponent(typeof (Text))]
+    [RequireComponent(typeof(TextMeshProUGUI))]
     public class FPSCounter : MonoBehaviour
     {
         const float fpsMeasurePeriod = 0.5f;
         private int m_FpsAccumulator = 0;
         private float m_FpsNextPeriod = 0;
+        private float m_MinFps;
+        private float m_MaxFps;
         private int m_CurrentFps;
         const string display = "{0} FPS";
-        private Text m_Text;
-
+        private TextMeshProUGUI m_Text;
+        private float currentTime;
+        private readonly StringBuilder m_StringBuilder = new StringBuilder(16);
 
         private void Start()
         {
             m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
-            m_Text = GetComponent<Text>();
+            m_Text = GetComponent<TextMeshProUGUI>();
         }
 
 
@@ -26,12 +31,29 @@ namespace Breezinstein.Tools
         {
             // measure average frames per second
             m_FpsAccumulator++;
-            if (Time.realtimeSinceStartup > m_FpsNextPeriod)
+            currentTime = Time.realtimeSinceStartup;
+            if (currentTime > m_FpsNextPeriod)
             {
-                m_CurrentFps = (int) (m_FpsAccumulator/fpsMeasurePeriod);
+                m_CurrentFps = (int)(m_FpsAccumulator / fpsMeasurePeriod);
                 m_FpsAccumulator = 0;
-                m_FpsNextPeriod += fpsMeasurePeriod;
-                m_Text.text = string.Format(display, m_CurrentFps);
+                m_FpsNextPeriod = currentTime + fpsMeasurePeriod;
+
+                if (m_CurrentFps < m_MinFps)
+                {
+                    m_MinFps = m_CurrentFps;
+                }
+                if (m_CurrentFps > m_MaxFps)
+                {
+                    m_MaxFps = m_CurrentFps;
+                }
+
+                //display the min, current and max fps on different lines in the same string
+                m_StringBuilder.Clear();
+                m_StringBuilder.AppendLine(string.Format(display, m_CurrentFps));
+                m_StringBuilder.AppendLine(string.Format(display, m_MinFps));
+                m_StringBuilder.AppendLine(string.Format(display, m_MaxFps));
+                m_Text.text = m_StringBuilder.ToString();
+
             }
         }
     }
